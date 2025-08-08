541513d95db954888e3e8653730b7e7c6ca58407
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2025-08-08 14:53:09 +0700

    s

--- app/Http/Controllers/Frontend/ContactController.php
+++ app/Http/Controllers/Frontend/ContactController.php
@@ -15,44 +15,44 @@ class ContactController extends Controller
     {
         return view('frontend.pages.contact.contact');
     }
-  public function submitContactForm(ContactRequest $request)
-{
-    $lastSubmitted = session('last_contact_submit');
-    if ($lastSubmitted && now()->diffInSeconds($lastSubmitted) < 60) {
-        return redirect()->back()->with('error', 'Bạn vừa gửi, vui lòng thử lại sau 1 phút.');
-    }
+    public function submitContactForm(ContactRequest $request)
+    {
+        // $lastSubmitted = session('last_contact_submit');
+        // if ($lastSubmitted && now()->diffInSeconds($lastSubmitted) < 60) {
+        //     return redirect()->back()->with('error', 'Bạn vừa gửi, vui lòng thử lại sau 1 phút.');
+        // }
 
-    DB::beginTransaction();
-    try {
-        $contact = Testimonial::create([
-            'name' => $request->name,
-            'message' => $request->message,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'rating' => $request->rating,
-        ]);
+        DB::beginTransaction();
+        try {
+            $contact = Testimonial::create([
+                'name' => $request->name,
+                'message' => $request->message,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'rating' => $request->rating,
+            ]);
 
-        DB::commit();
+            DB::commit();
 
-        if (!empty($contact->email)) {
-            Mail::to($contact->email)->send(new ContactMail($contact));
+            if (!empty($contact->email)) {
+                Mail::to($contact->email)->send(new ContactMail($contact));
+            }
+
+            session(['last_contact_submit' => now()]);
+
+            return redirect()->back()->with('success', 'Cảm ơn quý khách hàng đã phản hồi!');
+        } catch (\Throwable $e) {
+            DB::rollBack();
+
+            Log::error('Lỗi gửi form liên hệ', [
+                'error' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+                'request' => $request->all(),
+            ]);
+
+            return redirect()->back()->with('error', 'Đã có lỗi kỹ thuật xảy ra: ' . $e->getMessage());
         }
-
-        session(['last_contact_submit' => now()]);
-
-        return redirect()->back()->with('success', 'Cảm ơn quý khách hàng đã phản hồi!');
-    } catch (\Throwable $e) {
-        DB::rollBack();
-
-        Log::error('Lỗi gửi form liên hệ', [
-            'error' => $e->getMessage(),
-            'trace' => $e->getTraceAsString(),
-            'request' => $request->all(),
-        ]);
-
-        return redirect()->back()->with('error', 'Đã có lỗi kỹ thuật xảy ra: ' . $e->getMessage());
     }
-}
 
 
 

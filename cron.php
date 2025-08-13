c54c34b794080cf350c837587fb6260758addefa
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2025-08-13 15:48:03 +0700

    cronjob

--- cron.php
+++ cron.php
@@ -1,2 +1,17 @@
 <?php
-exec('/usr/bin/php /home/khoang2003/domains/app2.webnew.info.vn/public_html/artisan schedule:run');
+
+require __DIR__ . '/vendor/autoload.php';
+$app = require_once __DIR__ . '/bootstrap/app.php';
+
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Support\Facades\Log;
+
+// Khởi tạo kernel
+$kernel = $app->make(Kernel::class);
+
+try {
+    $kernel->call('schedule:run');
+    Log::info('[Cron] Scheduler chạy thành công lúc ' . now());
+} catch (\Exception $e) {
+    Log::error('[Cron] Lỗi khi chạy scheduler: ' . $e->getMessage());
+}

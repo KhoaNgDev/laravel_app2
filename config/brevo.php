20e0e090401d1367486ee1eda8b0162b014cf11c
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2025-08-13 10:41:50 +0700

    check

--- config/brevo.php
+++ config/brevo.php
@@ -16,7 +16,7 @@ return [
      * https://app.brevo.com/settings/keys/api
      */
 
-    'api_key' => env('BREVO_API_KEY', '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'),
+    'api_key' => env('BREVO_API_KEY', 'null'),
 
     'partner_key' => env('BREVO_PARTNER_KEY', null),
 
